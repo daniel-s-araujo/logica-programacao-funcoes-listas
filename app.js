a8fc1b3e02236e933f229b2bbc55359841542b8f
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-06-06 11:49:58 -0300

    Funções com parâmetros

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ function verificarChute() {
     console.log('o botão foi clicado');
 }
 
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
