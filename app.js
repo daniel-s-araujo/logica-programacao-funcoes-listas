9e4302eed41d5e77a31aeac304a50a55aba5f93c
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-06-07 09:59:36 -0300

    Limitando os números sorteados

--- app.js
+++ app.js
@@ -25,7 +25,12 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
 
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
@@ -55,6 +60,7 @@ function exibirMensagemInicial() {
 }
 
 let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 exibirMensagemInicial();
